b41efe5935861db30c99d7ea3e51ea026c8e6a5b
Vu Trinh Tu <vutulearning12@gmail.com>
2025-02-18 22:00:02 +0700

    Introduce python library and tool application

---
+++ Warm-up/lesson6/lesson6.py
@@ -0,0 +1,94 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "a17bd294-e656-4e1b-9bf0-0509ad764def",
+   "metadata": {},
+   "source": [
+    "# STREAMLIT TUTORIALS BASIC"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "d7f34e71-2716-4189-a500-e1cea73d7f0b",
+   "metadata": {},
+   "source": [
+    "# TEXT ELEMENTS"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "2bab6048-def3-4007-b747-7df825707d6f",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "2025-02-18 21:53:31.562 \n",
+      "  \u001b[33m\u001b[1mWarning:\u001b[0m to view this Streamlit app on a browser, run it with the following\n",
+      "  command:\n",
+      "\n",
+      "    streamlit run C:\\Users\\Admin\\anaconda3\\Lib\\site-packages\\ipykernel_launcher.py [ARGUMENTS]\n"
+     ]
+    },
+    {
+     "data": {
+      "text/plain": [
+       "DeltaGenerator()"
+      ]
+     },
+     "execution_count": 3,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "import streamlit as st\n",
+    "\n",
+    "st.title(\"MY PROJECT\")\n",
+    "st.header(\"This is a header\")\n",
+    "st.subheader(\"This is a subheader\")\n",
+    "st.text(\"I love AI VIET NAM\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "10bc37c0-d096-4202-bd68-b6b52cc81cc7",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "12126116-f79f-49c0-bd1c-3e1ab930e817",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:base] *",
+   "language": "python",
+   "name": "conda-base-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
